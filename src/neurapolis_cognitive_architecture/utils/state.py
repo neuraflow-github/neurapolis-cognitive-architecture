7ec72de1dd6d83af689ca8863b2bca70985e023d
Pascal <86606160+PascalNobereit@users.noreply.github.com>
2024-10-08 19:24:25 +0200

    arbitrary types in state

--- src/neurapolis_cognitive_architecture/utils/state.py
+++ src/neurapolis_cognitive_architecture/utils/state.py
@@ -2,12 +2,12 @@ from langgraph.graph import add_messages
 from langchain_core.messages import HumanMessage, BaseMessage
 from typing import TypedDict, Annotated, Sequence, Optional
 from neurapolis_retriever.models.date_filter import DateFilter
-from pydantic import Field
+from pydantic import Field, ConfigDict
 
 
 class FilteredBaseMessage(HumanMessage):
-
-    date_filter: Optional[DateFilter] = Field()
+    date_filter: Optional[DateFilter] = Field(default=None)
+    model_config = ConfigDict(arbitrary_types_allowed=True)
 
 
 class AgentState(TypedDict):
