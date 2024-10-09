442c0b10d38eb08877b99506ba368c3653eb93dc
Pascal <86606160+PascalNobereit@users.noreply.github.com>
2024-10-09 16:32:56 +0200

    remove try catch

--- src/neurapolis_cognitive_architecture/utils/nodes.py
+++ src/neurapolis_cognitive_architecture/utils/nodes.py
@@ -85,90 +85,86 @@ async def call_tool(state, config):
             else:
                 return default
 
-        try:
-            async for x_event in graph.astream_events(
-                State(
-                    query=query,
-                    date_filter=last_human_message.date_filter,
-                ),
-                include_names=retriever_step_values,
-                include_types=["on_chain_start", "on_chain_end"],
-                version="v1",
-            ):
-                event_name = x_event["name"]
-                try:
-                    retriever_step = RetrieverStep(event_name)
-                except:
+        async for x_event in graph.astream_events(
+            State(
+                query=query,
+                date_filter=last_human_message.date_filter,
+            ),
+            include_names=retriever_step_values,
+            include_types=["on_chain_start", "on_chain_end"],
+            version="v1",
+        ):
+            event_name = x_event["name"]
+            try:
+                retriever_step = RetrieverStep(event_name)
+            except:
+                continue
+            event_type = x_event["event"]
+            if event_type == "on_chain_start":
+                state = x_event["data"]["input"]
+                if type(state) != State:
                     continue
-                event_type = x_event["event"]
-                if event_type == "on_chain_start":
-                    state = x_event["data"]["input"]
-                    if type(state) != State:
-                        continue
-                    if retriever_step == RetrieverStep.FINISHER:
-                        continue
-                    loader_update = LoaderUpdate(
-                        retriever_step,
-                        state.search_count,
-                        state.hit_count,
-                        state.relevant_hit_count,
-                        [],
-                    )
-                    yield loader_update
-                elif event_type == "on_chain_end":
-                    state = x_event["data"]["output"]
-                    if type(state) != State:
-                        continue
-                    if retriever_step != RetrieverStep.FINISHER:
-                        continue
-                    file_infos = []
-                    if state:
-                        for x_search in get_attr_or_key(state, "searches", []):
-                            if not x_search:
+                if retriever_step == RetrieverStep.FINISHER:
+                    continue
+                loader_update = LoaderUpdate(
+                    retriever_step,
+                    state.search_count,
+                    state.hit_count,
+                    state.relevant_hit_count,
+                    [],
+                )
+                yield loader_update
+            elif event_type == "on_chain_end":
+                state = x_event["data"]["output"]
+                if type(state) != State:
+                    continue
+                if retriever_step != RetrieverStep.FINISHER:
+                    continue
+                file_infos = []
+                if state:
+                    for x_search in get_attr_or_key(state, "searches", []):
+                        if not x_search:
+                            continue
+                        x_hits = get_attr_or_key(x_search, "hits", [])
+                        for x_hit in x_hits:
+                            if not x_hit:
                                 continue
-                            x_hits = get_attr_or_key(x_search, "hits", [])
-                            for x_hit in x_hits:
-                                if not x_hit:
+                            x_grading = get_attr_or_key(x_hit, "grading")
+                            if not x_grading:
+                                continue
+                            is_relevant = get_attr_or_key(
+                                x_grading, "is_relevant", False
+                            )
+                            if is_relevant:
+                                try:
+                                    related_file = get_attr_or_key(
+                                        x_hit, "related_file", {}
+                                    )
+                                    file_info = FileInfo(
+                                        id=get_attr_or_key(related_file, "id"),
+                                        name=get_attr_or_key(related_file, "name"),
+                                        description=get_attr_or_key(
+                                            x_grading, "description"
+                                        ),
+                                        text=get_attr_or_key(x_hit, "text"),
+                                        created_at=get_attr_or_key(
+                                            related_file, "created"
+                                        ),
+                                        pdf_url=get_attr_or_key(
+                                            related_file, "download_url"
+                                        ),
+                                        highlight_areas=[],
+                                    )
+                                    file_infos.append(file_info)
+                                    if len(file_infos) >= 20:
+                                        break
+                                except Exception as e:
+                                    print(f"An error occurred: {e}")
                                     continue
-                                x_grading = get_attr_or_key(x_hit, "grading")
-                                if not x_grading:
-                                    continue
-                                is_relevant = get_attr_or_key(
-                                    x_grading, "is_relevant", False
-                                )
-                                if is_relevant:
-                                    try:
-                                        related_file = get_attr_or_key(
-                                            x_hit, "related_file", {}
-                                        )
-                                        file_info = FileInfo(
-                                            id=get_attr_or_key(related_file, "id"),
-                                            name=get_attr_or_key(related_file, "name"),
-                                            description=get_attr_or_key(
-                                                x_grading, "description"
-                                            ),
-                                            text=get_attr_or_key(x_hit, "text"),
-                                            created_at=get_attr_or_key(
-                                                related_file, "created"
-                                            ),
-                                            pdf_url=get_attr_or_key(
-                                                related_file, "download_url"
-                                            ),
-                                            highlight_areas=[],
-                                        )
-                                        file_infos.append(file_info)
-                                        if len(file_infos) >= 20:
-                                            break
-                                    except Exception as e:
-                                        print(f"An error occurred: {e}")
-                                        continue
-                            if len(file_infos) >= 20:
-                                break
-                        yield file_infos
-                        break
-        except Exception as e:
-            print(f"An error occurred during file retrieval: {e}")
-            yield []
+                        if len(file_infos) >= 20:
+                            break
+                    yield file_infos
+                    break
 
     file_infos = []
     async for result in get_file_infos(query):
