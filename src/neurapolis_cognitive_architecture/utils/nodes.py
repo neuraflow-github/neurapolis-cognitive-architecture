10f45552fae3ce66a2582c274d4e585b7d59faa9
Pascal <86606160+PascalNobereit@users.noreply.github.com>
2024-10-10 19:45:38 +0200

    new parsing

--- src/neurapolis_cognitive_architecture/utils/nodes.py
+++ src/neurapolis_cognitive_architecture/utils/nodes.py
@@ -53,126 +53,40 @@ async def call_tool(state, config):
     tool_call = last_message.tool_calls[0]
     query = tool_call["args"]["full_query"]
 
-    async def get_file_hits(query):
-        from datetime import datetime
-        from typing import AsyncIterator, Optional
+    from neurapolis_retriever.main import NeurapolisRetriever
+    from neurapolis_retriever.models.date_filter import DateFilter
+    from neurapolis_retriever.models.file_hit import FileHit
+    from neurapolis_retriever.models.loader_update import LoaderUpdate
+    from neurapolis_retriever.models.quality_preset import QualityPreset
 
-        from neurapolis_retriever.graph import graph
-        from neurapolis_retriever.models.date_filter import DateFilter
-        from neurapolis_retriever.models.file_hit import FileHit
-        from neurapolis_retriever.models.loader_update import LoaderUpdate
-        from neurapolis_retriever.models.retriever_step import RetrieverStep
-        from neurapolis_retriever.models.state import State
-        from neurapolis_retriever.retriever_config import retriever_config
-
-        retriever_config.planner_vector_search_limit = 1
-        retriever_config.planner_keyword_search_limit = 1
-        retriever_config.planner_min_relevant_hit_count = 1
-        retriever_config.planner_max_search_count = 1
-        retriever_config.sub_planner_keyword_search_limit = 1
-        retriever_config.sub_planner_relevant_hits_limit = 1
-        retriever_config.initial_retriever_vector_search_top_k = 30
-        retriever_config.reranked_retriever_vector_search_top_k = 5
-        retriever_config.initial_retriever_keyword_search_top_k = 30
-        retriever_config.reranked_retriever_keyword_search_top_k = 5
-
-        retriever_step_values = [step.value for step in RetrieverStep]
-
-        def get_attr_or_key(obj, attr, default=None):
-            if hasattr(obj, attr):
-                return getattr(obj, attr)
-            elif isinstance(obj, dict) and attr in obj:
-                return obj[attr]
-            else:
-                return default
-
-        async for x_event in graph.astream_events(
-            State(
-                query=query,
-                date_filter=last_human_message.date_filter,
-            ),
-            include_names=retriever_step_values,
-            include_types=["on_chain_start", "on_chain_end"],
-            version="v1",
-        ):
-            event_name = x_event["name"]
-            try:
-                retriever_step = RetrieverStep(event_name)
-            except:
-                continue
-            event_type = x_event["event"]
-            if event_type == "on_chain_start":
-                state = x_event["data"]["input"]
-                if type(state) != State:
-                    continue
-                if retriever_step == RetrieverStep.FINISHER:
-                    continue
-                loader_update = LoaderUpdate(
-                    retriever_step,
-                    state.search_count,
-                    state.hit_count,
-                    state.relevant_hit_count,
-                    [],
-                )
-                yield loader_update
-            elif event_type == "on_chain_end":
-                state = x_event["data"]["output"]
-                if type(state) != State:
-                    continue
-                if retriever_step != RetrieverStep.FINISHER:
-                    continue
-                file_hits = []
-                if state:
-                    for x_search in get_attr_or_key(state, "searches", []):
-                        if not x_search:
-                            continue
-                        x_hits = get_attr_or_key(x_search, "hits", [])
-                        for x_hit in x_hits:
-                            if not x_hit:
-                                continue
-                            x_grading = get_attr_or_key(x_hit, "grading")
-                            if not x_grading:
-                                continue
-                            is_relevant = get_attr_or_key(
-                                x_grading, "is_relevant", False
-                            )
-                            if is_relevant:
-                                try:
-                                    related_file = get_attr_or_key(
-                                        x_hit, "related_file", {}
-                                    )
-                                    file_hit = FileHit(
-                                        id=get_attr_or_key(related_file, "id"),
-                                        name=get_attr_or_key(related_file, "name"),
-                                        description=get_attr_or_key(
-                                            x_grading, "description"
-                                        ),
-                                        text=get_attr_or_key(x_hit, "text"),
-                                        created_at=get_attr_or_key(
-                                            related_file, "created"
-                                        ),
-                                        pdf_url=get_attr_or_key(
-                                            related_file, "download_url"
-                                        ),
-                                        highlight_areas=[],
-                                    )
-                                    file_hits.append(file_hit)
-                                    if len(file_hits) >= 20:
-                                        break
-                                except Exception as e:
-                                    print(f"An error occurred: {e}")
-                                    continue
-                        if len(file_hits) >= 20:
-                            break
-                    yield file_hits
-                    break
-
+    retriever = NeurapolisRetriever()
+    date_filter = last_human_message.date_filter
     file_hits = []
-    async for result in get_file_hits(query):
-        if isinstance(result, list):
-            file_hits = result
+
+    async for x_event in retriever.retrieve(
+        query,
+        date_filter,
+        QualityPreset.LOW,
+    ):
+        if isinstance(x_event, LoaderUpdate):
+            loader_update = x_event
+            print("---")
+            print(f"Retriever Step: {loader_update.retriever_step}")
+            print(f"Search Count: {loader_update.search_count}")
+            print(f"Hit Count: {loader_update.hit_count}")
+            print(f"Relevant Hit Count: {loader_update.relevant_hit_count}")
+            print("Log Entries:")
+            for x_log_entry in loader_update.log_entries:
+                print(f"  - {x_log_entry.text}")
+            print("---")
+        elif isinstance(x_event, list):
+            file_hits = x_event
+            print("---")
+            print("Retrieved File Infos:")
+            for x_file_hit in file_hits:
+                print(f"  - {x_file_hit.name}: {x_file_hit.description}")
+            print("---")
             break
-        # Handle LoaderUpdate if needed
 
     # Serialize file_hits before adding to the content
     serialized_file_hits = [file_hit.model_dump() for file_hit in file_hits]
