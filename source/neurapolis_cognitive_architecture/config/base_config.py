8b5029e1dae6de627853053c4a3586d4c917b954
Julius Huck <mail@juliushuck.com>
2024-12-10 02:29:56 +0100

    Update

--- source/neurapolis_cognitive_architecture/config/base_config.py
+++ source/neurapolis_cognitive_architecture/config/base_config.py
@@ -3,7 +3,8 @@ from pydantic_settings import BaseSettings
 
 
 class BaseConfig(BaseSettings):
-    reference_limit: int = Field(10)
+    reference_limit: int = Field(20)
+    llm_context_reference_limit: int = Field(10)
     context_window_token_limit: int = Field(
         150_0000
     )  # 200k would be the max for Claude 3.5 Sonnet
